bea3c88087bc015679b7d48a49bcc5a6a79c7f80
nikitkachan <nikyc777@gmail.com>
2024-01-14 14:22:20 +0200

    готово

--- schemas/addSchema.js
+++ schemas/addSchema.js
@@ -4,6 +4,7 @@ const addSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().required(),
   phone: Joi.number().required(),
+  favorite: Joi.boolean(),
 });
 
 module.exports = addSchema;
